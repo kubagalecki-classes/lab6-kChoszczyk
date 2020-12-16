ccd37dfc2b73d72bd4040aea19049c2c3e93e5d7
kChoszczyk <krzysiex97@gmail.com>
2020-12-16 09:16:20 +0000

    Zal pr 1

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,46 @@
+#include "make_random_vector.hpp"
+#include <functional>
 #include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    puts("Hello, World!");
+
+    /*
+    auto vec = make_random_vector(15, 0, 10);
+    cout << "Losowy" << endl;
+    for (auto e : vec)
+        cout << e << endl;
+    vec.erase(remove(vec.begin(), vec.end(), 3), vec.end());
+    cout << "Posortowany" << endl;
+    for (auto e : vec)
+        cout << e << endl;*/
+
+    /*string kom;
+    // string pie;
+    bool cos;
+    cin >> kom;
+    reverse(kom.begin(), kom.end());
+    // if (i1 == kom.end())
+    //   cos = 0;
+    // else
+    //    cos = 1;
+    cout << kom << endl;*/
+
+    // Zadanie przed cwiczeniami
+    /*int  w;
+    auto vec = make_random_vector(20, 0, 10);
+    cout << "Losowy" << endl;
+    for (auto e : vec)
+        cout << e << endl;
+    sort(vec.begin(), vec.end(), greater< int >());
+    cout << "Posortowany" << endl;
+    for (auto e : vec)
+        cout << e << endl;
+    cin >> w;
+    auto c = count_if(vec.begin(), vec.end(), [&](int x) { return x > w; });
+    cout << "Liczba policzonych siódemek " << c << endl;*/
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,8 +9,16 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-
     // Twoja implementacja tutaj
+    
+    std::for_each(people.begin(), people.end(), [](Human& pp) { pp.birthday(); });
+
+    std::transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human pp) {
+        if (pp.isMonster())
+            return 'n';
+        else
+            return 'y';
+    });
 
     return ret_v;
 }
